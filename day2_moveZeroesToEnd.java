6d5fc430e7a0cf9220dcddf6db41704d9b8523af
Aasritha Sai Kollipara <113545399+aasritha04@users.noreply.github.com>
2025-01-03 18:33:44 +0530

    Update day2_moveZeroesToEnd.java

--- day2_moveZeroesToEnd.java
+++ day2_moveZeroesToEnd.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    void pushZerosToEnd(int[] arr) 
+    {
+        int n=arr.length,ind=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=0)
+            {
+                int temp=arr[i];
+                arr[i]=arr[ind];
+                arr[ind]=temp;
+                ind++;
+            }
+        }
+        
+    }
+}
